bd88b184d64524a07ba691b48705ffa4f0567acd
Victor Rivera <verm2405@gmail.com>
2018-11-04 10:41:43 -0500

    Initial set up of new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Titles from "./Titles";
+import Form from "./Form";
+import Weather from "./Weather";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
