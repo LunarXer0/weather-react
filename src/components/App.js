bbc4f9a69ac2a710fe9c7257186df0070809ef62
Victor Rivera <verm2405@gmail.com>
2018-11-04 10:37:37 -0500

    Scrapping the entire project. Taking it in a new direction

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Layout } from "antd";
 
-import MyHeader from "./header-component";
-import Home from "./content-component";
-import { getWeatherByCityAndCC, getForecastForFiveDays } from "../utils/api";
-
-import "antd/dist/antd.css";
-
-class App extends Component {
-  state = {
-    location: {
-      city: "",
-      countryCode: ""
-    }
-  };
-
-  handleLocationInput = location => {
-    let city, countryCode;
-    if (location.includes(",")) {
-      city = location.split(",")[0];
-      countryCode = location.split(",")[1].trim();
-    }
-    if ((city !== "" || null) && (countryCode !== "" || null)) {
-      this.setState({
-        location: {
-          city,
-          countryCode
-        }
-      });
-    }
-  };
-
-  getWeather = () => {
-    getWeatherByCityAndCC(
-      this.state.location.city,
-      this.state.location.countryCode
-    );
-  };
-
-  getFiveDayForecast = () => {
-    getForecastForFiveDays(
-      this.state.location.city,
-      this.state.location.countryCode
-    );
-  };
-
+export default class App extends Component {
   render() {
-    const { Header, Content } = Layout;
     return (
-      <BrowserRouter>
-        <Layout>
-          <Header>
-            <MyHeader
-              title="Weather App"
-              handleLocationInput={this.handleLocationInput}
-              getWeather={this.getWeather}
-            />
-          </Header>
-          <Content style={{ height: "100vh", paddingTop: "15px" }}>
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={() => (
-                  <Home
-                    title="Enter a City and Country Code"
-                    handleLocationInput={this.handleLocationInput}
-                    getWeather={this.getWeather}
-                    getFiveDayForecast={this.getFiveDayForecast}
-                    city={this.state.location.city}
-                    countryCode={this.state.location.countryCode}
-                  />
-                )}
-              />
-              <Route
-                path="/forecast"
-                render={() => <Home title="Five Day Forecast" />}
-              />
-            </Switch>
-          </Content>
-        </Layout>
-      </BrowserRouter>
+      <div>
+        <h1>Hello</h1>
+      </div>
     );
   }
 }
-
-export default App;
